a85940c1f1565da9a5ef5b7337e235543bc53423
mdnajmul <mdnajmulovi@gmail.com>
2022-01-02 05:09:48 +0600

    Testimonial section complete by Najmul Ovi

--- src/pages/Home/Home/Home.js
+++ src/pages/Home/Home/Home.js
@@ -2,12 +2,14 @@ import React from "react";
 import Footer from "../../Footer/Footer";
 import Header from "../../Header/Header";
 import Navigation from "../../Shared/Navigation/Navigation";
+import Testimonials from "../Testimonials/Testimonials";
 
 const Home = () => {
   return (
     <div>
       <Navigation />
       <Header></Header>
+      <Testimonials />
       <Footer></Footer>
     </div>
   );
