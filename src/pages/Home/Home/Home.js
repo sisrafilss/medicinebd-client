771e68c34c8d76713de3dbc37f951d79c367f537
Israfil Hossen <sisrafilss@gmail.com>
2022-01-01 19:54:28 +0600

    react-redux dependency added

--- src/pages/Home/Home/Home.js
+++ src/pages/Home/Home/Home.js
@@ -5,7 +5,7 @@ import Navigation from "../../Shared/Navigation/Navigation";
 const Home = () => {
   return (
     <div>
-        <Navigation />
+      <Navigation />
       <h2 className="text-center display-2 my-5">Welcome to MedicineBD</h2>
       <h2>This is Home Page!</h2>
       <Footer />
