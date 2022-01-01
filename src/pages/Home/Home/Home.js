5ea63adbd38e26ab8573257185105178ba88c19e
mdnajmul <mdnajmulovi@gmail.com>
2022-01-02 03:11:53 +0600

    Newsletter section design comlete by Najmul Ovi

--- src/pages/Home/Home/Home.js
+++ src/pages/Home/Home/Home.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Footer from "../../Shared/Footer/Footer/Footer";
 import Navigation from "../../Shared/Navigation/Navigation";
+import Newsletter from "../Newsletter/Newsletter";
 
 const Home = () => {
   return (
     <div>
-        <Navigation />
+      <Navigation />
+      <Newsletter />
       <h2 className="text-center display-2 my-5">Welcome to MedicineBD</h2>
       <h2>This is Home Page!</h2>
       <Footer />
