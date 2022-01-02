72d473d2b9514a2fee8623b6f2f8b8f02b0509a6
Israfil Hossen <sisrafilss@gmail.com>
2022-01-02 15:32:56 +0600

    privat route added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Home from "./pages/Home/Home/Home";
 import Login from "./pages/Login/Login/Login";
 import PageNotFound from "./pages/PageNotFound/PageNotFound";
 import Register from "./pages/Login/Register/Register";
+import PrivatRoute from "./PrivatRoute/PrivatRoute";
+import Dashboard from "./pages/Dashboard/Dashboard/Dashboard";
 
 function App() {
   return (
@@ -13,6 +15,14 @@ function App() {
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
       <Route path="*" element={<PageNotFound />} />
+      <Route
+        path="/dashboard"
+        element={
+          <PrivatRoute>
+            <Dashboard />
+          </PrivatRoute>
+        }
+      />
     </Routes>
   );
 }
