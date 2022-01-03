fd8f96d4098abefa6ddcae4be9fb96a1240c6288
Israfil Hossen <sisrafilss@gmail.com>
2022-01-03 08:56:47 +0600

    contact form initialize

---
+++ src/pages/Home/ContactForm/ContactForm.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ContactForm = () => {
+  return (
+    <div className="container">
+      <h2>This is Contact form</h2>
+    </div>
+  );
+};
+
+export default ContactForm;
