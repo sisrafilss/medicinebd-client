c11141e87e196bb6d8847e97a07303b9944eaa10
mdnajmul <mdnajmulovi@gmail.com>
2022-01-03 16:11:32 +0600

    service summary page removed from main branch home page by najmul ovi

--- src/pages/Home/Home/Home.js
+++ src/pages/Home/Home/Home.js
@@ -5,7 +5,6 @@ import Newsletter from "../Newsletter/Newsletter";
 import HomeProducts from "../HomeProducts/HomeProducts";
 import Footer from "../../Footer/Footer";
 import Testimonials from "../Testimonials/Testimonials";
-import ServiceSummary from "../ServiceSummary/ServiceSummary";
 
 const Home = () => {
   return (
