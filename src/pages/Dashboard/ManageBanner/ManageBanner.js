b3769b069c48a26162f68075dce040a3759b5985
Israfil Hossen <sisrafilss@gmail.com>
2022-01-03 23:42:07 +0600

    Add slide modal added

--- src/pages/Dashboard/ManageBanner/ManageBanner.js
+++ src/pages/Dashboard/ManageBanner/ManageBanner.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { useForm } from "react-hook-form";
 import SingleBanner from "./SingleBanner";
 
 const bannerData = [
@@ -28,6 +29,31 @@ const bannerData = [
 const ManageBanner = () => {
   let slideNumber = 0;
 
+  // React Hook Form
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => {
+    const { category, name, description, image, price } = data;
+    const product = {
+      category,
+      name,
+      image,
+      price,
+      description: description.split("\n"),
+    };
+    console.log(product);
+    //   dispatch(addRentFlat(saleFlatInfo));
+
+    if (true) {
+      alert("Product Added Successfully");
+      reset();
+    }
+  };
+
   return (
     <div>
       <div className="mb-4">
@@ -45,10 +71,96 @@ const ManageBanner = () => {
         </div>
 
         <div className="text-center mt-5">
-          <button className="btn btn-lg btn-primary">
+          <button
+            className="btn btn-lg btn-primary"
+            type="button"
+            data-bs-toggle="modal"
+            data-bs-target="#exampleModal"
+          >
             {" "}
             <i class="fa fa-plus"></i> Add Slide
           </button>
+        </div>
+      </div>
+
+      <div
+        class="modal fade mt-5"
+        id="exampleModal"
+        tabindex="-1"
+        aria-labelledby="exampleModalLabel"
+        aria-hidden="true"
+      >
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="exampleModalLabel">
+                Add Slide
+              </h5>
+              <button
+                type="button"
+                class="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div class="modal-body">
+              <form onSubmit={handleSubmit(onSubmit)}>
+                <div className="mb-3">
+                  <label className="form-label">Title</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    placeholder="Slide Title"
+                    {...register("title", { required: true })}
+                  />
+                  {errors.name && (
+                    <span className="text-danger">
+                      Please enter a short title
+                    </span>
+                  )}
+                </div>
+
+                <div className="mb-3">
+                  <label className="form-label">Description</label>
+                  <textarea
+                    className="form-control"
+                    rows="3"
+                    placeholder="Enter a short description"
+                    {...register("description", { required: true })}
+                  ></textarea>
+                  {errors.description && (
+                    <span className="text-danger">
+                      Please enter a short description
+                    </span>
+                  )}
+                </div>
+
+                <div className="mt-4">
+                  <span className="mb-2 d-inline-block">Upload Image</span>
+                  <div class="input-group mb-4">
+                    <input
+                      type="file"
+                      class="form-control"
+                      id="inputGroupFile02"
+                      {...register("image", { required: true })}
+                    />
+                    <label class="input-group-text" htmlFor="inputGroupFile02">
+                      Upload
+                    </label>
+                  </div>
+                  {errors.image && (
+                    <span className="text-danger">Please choose an image</span>
+                  )}
+                </div>
+
+                <input
+                  type="submit"
+                  className="btn btn-primary fw-bold ml-auto"
+                  value="Add"
+                />
+              </form>
+            </div>
+          </div>
         </div>
       </div>
     </div>
