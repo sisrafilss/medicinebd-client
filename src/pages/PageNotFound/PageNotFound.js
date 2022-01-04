199ec6371d4d6456231a973a3be3b4f3b68ec959
showravdas11 <showravdas11@gmail.com>
2022-01-04 18:35:43 +0600

    notfound-page done

--- src/pages/PageNotFound/PageNotFound.js
+++ src/pages/PageNotFound/PageNotFound.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import video from '../../404 error lost in space.mp4';
+import { Link } from "react-router-dom";
+import './PageNotFound.css'
 
 const PageNotFound = () => {
     return (
         <div>
-            <h1>404</h1>
-            <h2>Page Not Found!</h2>
+            <div className='bg-vdo'>
+                <video autoPlay loop muted className='bg-vdo' src={video} type='video/mp4'></video>
+            </div>
+            <div className='d-flex justify-content-center mb-5'>
+                <button className='back-btn'><Link className='not-link' to="/home">Back To Home</Link></button>
+
+            </div>
         </div>
     );
 };
