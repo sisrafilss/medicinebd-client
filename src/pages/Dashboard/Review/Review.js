31a4a89426e402ab45d049d563ce08276bfb435c
Israfil Hossen <sisrafilss@gmail.com>
2022-01-04 12:34:44 +0600

    review component design completed with redux store connected

--- src/pages/Dashboard/Review/Review.js
+++ src/pages/Dashboard/Review/Review.js
@@ -1,16 +1,19 @@
 import React, { useState } from "react";
 import { useForm } from "react-hook-form";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import useAuth from "../../../hooks/useAuth";
 import ReactStars from "react-rating-stars-component";
+import { setReviewSubmitedStatus } from "../../../store/userDashboard";
 
 const Review = () => {
   const { user } = useAuth();
 
+  const dispatch = useDispatch();
+
   // Get current review submitted status from store
-  //   const reviewSubmited = useSelector(
-  //     (state) => state.entities.userDashboard.reviewSubmited
-  //   );
+  const reviewSubmited = useSelector(
+    (state) => state.entities.userDashboard.reviewSubmited
+  );
 
   const [rating, setRating] = useState(null);
 
@@ -39,79 +42,79 @@ const Review = () => {
   };
 
   return (
-    <div className="mt-5 container" style={style.reviewContainer}>
+    <div className="mt-4 container" style={style.reviewContainer}>
       <div className="login-form border rounded">
-        {/* {reviewSubmited && ( */}
-        <div>
-          <div
-            className="alert alert-success alert-dismissible fade show"
-            role="alert"
-          >
-            Review Submited.
-            <button
-              // onClick={() =>
-              //   dispatch(setReviewSubmitedStatus({ state: true }))
-              // }
-              type="button"
-              className="btn-close"
-              data-bs-dismiss="alert"
-              aria-label="Close"
-            ></button>
-          </div>
+        {reviewSubmited && (
           <div>
-            <button
-              // onClick={() =>
-              //   dispatch(setReviewSubmitedStatus({ state: false }))
-              // }
-              className="btn btn-success"
+            <div
+              className="alert alert-success alert-dismissible fade show"
+              role="alert"
             >
-              Write Another Review
-            </button>
+              Review Submited.
+              <button
+                onClick={() =>
+                  dispatch(setReviewSubmitedStatus({ state: true }))
+                }
+                type="button"
+                className="btn-close"
+                data-bs-dismiss="alert"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div>
+              <button
+                onClick={() =>
+                  dispatch(setReviewSubmitedStatus({ state: false }))
+                }
+                className="btn btn-success"
+              >
+                Write Another Review
+              </button>
+            </div>
           </div>
-        </div>
-        {/* )} */}
+        )}
 
-        {/* {!reviewSubmited && ( */}
-        <div>
+        {!reviewSubmited && (
           <div>
-            <h2 className="h3 mb-4">Write a Review</h2>
+            <div>
+              <h2 className="h3 mb-4">Write a Review</h2>
+            </div>
+
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <div className="mb-4">
+                <ReactStars
+                  count={5}
+                  size={24}
+                  onChange={ratingChanged}
+                  isHalf={true}
+                  emptyIcon={<i className="far fa-star"></i>}
+                  halfIcon={<i className="fa fa-star-half-alt"></i>}
+                  fullIcon={<i className="fa fa-star"></i>}
+                  activeColor="rgb(250, 202, 81)"
+                />
+              </div>
+
+              <div className="mb-3">
+                <textarea
+                  rows="4"
+                  cols="50"
+                  type="text"
+                  className="form-control"
+                  {...register("review", { required: true })}
+                />
+                {errors.review && (
+                  <span className="text-danger">This field is required</span>
+                )}
+              </div>
+
+              <input
+                type="submit"
+                className="btn btn-primary fw-bold"
+                value="Submit"
+              />
+            </form>
           </div>
-
-          <form onSubmit={handleSubmit(onSubmit)}>
-            <div className="mb-4">
-              <ReactStars
-                count={5}
-                size={24}
-                onChange={ratingChanged}
-                isHalf={true}
-                emptyIcon={<i className="far fa-star"></i>}
-                halfIcon={<i className="fa fa-star-half-alt"></i>}
-                fullIcon={<i className="fa fa-star"></i>}
-                activeColor="rgb(250, 202, 81)"
-              />
-            </div>
-
-            <div className="mb-3">
-              <textarea
-                rows="4"
-                cols="50"
-                type="text"
-                className="form-control"
-                {...register("review", { required: true })}
-              />
-              {errors.review && (
-                <span className="text-danger">This field is required</span>
-              )}
-            </div>
-
-            <input
-              type="submit"
-              className="btn btn-primary fw-bold"
-              value="Submit"
-            />
-          </form>
-        </div>
-        {/* )} */}
+        )}
       </div>
     </div>
   );
