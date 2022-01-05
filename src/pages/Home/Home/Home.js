6b34863ab84f451ddae9a3c1a7def218ce4d7634
Syed Mahadi Hasan Opu <smhasanopu@gmail.com>
2022-01-05 10:54:45 +0600

    commit frist

--- src/pages/Home/Home/Home.js
+++ src/pages/Home/Home/Home.js
@@ -1,13 +1,17 @@
+import { Grid, Item } from "@mui/material";
 import React from "react";
 import Footer from "../../Shared/Footer/Footer/Footer";
 import Navigation from "../../Shared/Navigation/Navigation";
+import Prescription from "../Prescription/Prescription";
+
 
 const Home = () => {
   return (
     <div>
-        <Navigation />
+      <Navigation />
       <h2 className="text-center display-2 my-5">Welcome to MedicineBD</h2>
       <h2>This is Home Page!</h2>
+      <Prescription />
       <Footer />
     </div>
   );
