452cd9a6e8fd9e388ab0a0daf8794f1f48acbab5
Israfil Hossen <sisrafilss@gmail.com>
2022-01-05 17:39:27 +0600

    allProduct page added

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import ViewAllProduct from "./pages/ViewAllProduct/ViewAllProduct";
 import Register from "./pages/Login/Register/Register";
 import PrivatRoute from "./PrivatRoute/PrivatRoute";
 import Dashboard from "./pages/Dashboard/Dashboard/Dashboard";
+import AllProduct from "./pages/AllProduct/AllProduct";
 
 function App() {
   return (
@@ -15,7 +16,8 @@ function App() {
       <Route path="/home" element={<Home />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
-      <Route path="/allproduct" element={<ViewAllProduct />} />
+      <Route path="/allproduct" element={<AllProduct />} />
+
       <Route path="*" element={<PageNotFound />} />
       
       <Route
